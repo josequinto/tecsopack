a728f90e483f98dab49edf3901941ab0db6cab05
josequinto <136132922+josequinto@users.noreply.github.com>
2024-01-18 15:02:06 -0500

    PruebaDeploy

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import Logo from "../assets/Logo_blanco.png";
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <img src={Logo} alt="" />
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Inicio.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./NavBar";
+
+const Inicio = () => {
+  return (
+    <section id="inicio">
+      <NavBar />
+      <h1>
+        Haz del mundo un <strong>lugar mejor </strong>
+        <br /> un <strong>envoltorio</strong> a la vez.
+      </h1>
+    </section>
+  );
+};
+
+export default Inicio;
